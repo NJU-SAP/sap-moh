34397c709c622b8d1d170eaca1f5912f6ce151d4
Henry Li <henry1943@163.com>
2017-05-08 15:00:03 +0800

    Remove zoom control

--- src/nju/map/MapView.js
+++ src/nju/map/MapView.js
@@ -55,7 +55,7 @@ export default class MapView extends View {
 
   _initMap() {
     const options = {
-      zoomControl: true,
+      zoomControl: false,
       attributionControl: false,
       center: this.getDefaultCenterLocation(),
       zoom: this.getDefaultZoom(),
