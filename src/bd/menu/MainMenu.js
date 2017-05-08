a43d606b9939e20bffd56d3d208721a234a3cbec
CompileYouth <CompileYouth@163.com>
2017-05-08 14:08:34 +0800

    Modify MainMenu.js.

--- src/bd/menu/MainMenu.js
+++ src/bd/menu/MainMenu.js
@@ -1,5 +1,61 @@
 import View from 'nju/view/View';
 
 export default class MainMenu extends View {
+  metadata = {
+    aggregations: {
+      subviews: { type: "bd.menu.MenuItem" }
+    },
 
+    events: {
+    	reset: {
+
+    	}
+    }
+  }
+
+  init() {
+    super.init();
+    this.addStyleClass("bd-main-menu");
+    this.addStyleClass("col-2 bottom-1 left-8");
+
+    var $menu = $("<menu><li class=placeholder><span class=diamond></span></li></menu>");
+    var self = this;
+    $menu.children(".placeholder").on("click", function() {
+    	self.fireReset();
+    });
+    this.$element.append($menu);
+    this.$container = $menu;
+  }
+
+  addSubview(subview) {
+    super.addSubview(subview);
+    var index = this.getSubviews().length;
+
+    switch (index) {
+      case 1:
+        subview.addStyleClass("row-1 center");
+        break;
+      case 2:
+        subview.addStyleClass("row-2 left");
+        break;
+      case 3:
+        subview.addStyleClass("row-2 right");
+        break;
+      case 4:
+        subview.addStyleClass("row-3 center");
+        break;
+      case 5:
+        subview.addStyleClass("row-4 left");
+        break;
+      case 6:
+        subview.addStyleClass("row-4 right");
+        break;
+      case 7:
+        subview.addStyleClass("row-5 start");
+        break;
+      case 8:
+        subview.addStyleClass("row-5 end");
+        break;
+    }
+  }
 }
