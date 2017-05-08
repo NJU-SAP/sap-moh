9f93b3578874d132c85a48d17e179bd559acdbe7
Henry Li <henry1943@163.com>
2017-05-08 10:55:02 +0800

    Update

--- src/sap/a/layout/FlowLayout.js
+++
@@ -1,9 +0,0 @@
-import Layout from "./Layout";
-
-export default class FlowLayout extends Layout
-{
-    append($child)
-    {
-        this.$element.append($child);
-    }
-}
--- src/sap/a/model/Model.js
+++
@@ -1,6 +0,0 @@
-import JSONModel from "sap/ui/model/json/JSONModel";
-
-export default class Model extends JSONModel
-{
-    
-}
