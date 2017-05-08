bc26015672d8adc9cc6909e72544aca284483472
Henry Li <henry1943@163.com>
2017-05-08 14:24:14 +0800

    Add tiles

---
+++ src/moh/tile/CounterTile.js
@@ -0,0 +1,25 @@
+import StandardTile from 'bd/tile/StandardTile';
+
+
+export default class CounterTile extends StandardTile {
+  metadata = {
+    properties: {
+      speedIndex: { type: 'object', bindable: true }
+    }
+  }
+
+  init() {
+    super.init();
+    this.setTitle('Buses');
+    this.setDescription('');
+    this.setUnit('');
+    this.setValueFormat('0');
+    // this.bindSpeedIndex({
+    //   model: 'index',
+    //   path: '/cityIndex'
+    // });
+    // this.attachEventOnce('addedToParent', () => {
+    //   self._initChart();
+    // });
+  }
+}
