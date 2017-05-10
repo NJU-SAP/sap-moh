ea7f4b67db9720abc4389d4f69ad02a762d638ec
Henry Li <henry1943@163.com>
2017-05-10 14:06:44 +0800

    Add stations

--- src/moh/model/GisModel.js
+++ src/moh/model/GisModel.js
@@ -6,11 +6,17 @@ import GisServiceClient from '../service/GisServiceClient';
 export default class GisModel extends Model {
   async initialLoad() {
     await this._loadEdges();
+    await this._loadStations();
     this.fireEvent('initialLoadCompleted');
   }
 
   async _loadEdges() {
     const edges = await GisServiceClient.getInstance().getEdges();
     this.setProperty('/edges', edges);
+  }
+
+  async _loadStations() {
+    const stations = await GisServiceClient.getInstance().getStations();
+    this.setProperty('/stations', stations);
   }
 }
