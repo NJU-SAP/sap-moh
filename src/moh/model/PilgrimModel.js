3108e3e052049e7d838f3e4450e4e1801746723c
Henry Li <henry1943@163.com>
2017-05-12 10:30:21 +0800

    Update

--- src/moh/model/PilgrimModel.js
+++ src/moh/model/PilgrimModel.js
@@ -1,18 +1,15 @@
 import Model from 'nju/model/Model';
 import StateBus from 'nju/state/StateBus';
 
+import PilgrimServiceClient from '../service/PilgrimServiceClient';
 
 
 export default class PilgrimModel extends Model {
   constructor() {
     super();
 
-    //StateBus.getInstance().bindState('timestamp').attachChange(this._onStateChange.bind(this));
+    StateBus.getInstance().bindState('timestamp').attachChange(this._onStateChange.bind(this));
   }
-
-  async initialLoad() {
-  }
-
 
   async checkStates() {
     const timestamp = StateBus.getInstance().getState('timestamp');
@@ -21,10 +18,14 @@ export default class PilgrimModel extends Model {
   }
 
   _onStateChange() {
-    this.checkStates();
+    if (StateBus.getInstance().getState('kaaba')) {
+      // Active only in Kaaba mode
+      this.checkStates();
+    }
   }
 
   async updateHeatmap(timestamp) {
-
+    const heatmap = await PilgrimServiceClient.getInstance().getHeatmap(timestamp);
+    this.setProperty('/heatmap', heatmap);
   }
 }
