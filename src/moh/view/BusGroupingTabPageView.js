a078aaf6f9f34ab5853601d4415e36a887bf4179
CompileYouth <CompileYouth@163.com>
2017-05-15 16:30:32 +0800

    modify busgrouptabpage style

--- src/moh/view/BusGroupingTabPageView.js
+++ src/moh/view/BusGroupingTabPageView.js
@@ -7,4 +7,25 @@ export default class BusGroupingTabPageView extends TabPageView {
       title: { type: 'string', defaultValue: 'Grouping' }
     }
   }
+
+  init() {
+    super.init();
+
+    this.addStyleClass('moh-view-busGroupingTabPageView');
+
+    this._initMain();
+  }
+
+  _initMain() {
+    const $info = $('<div class="info h3">Pilgrim Grouping Information Page</div>');
+    this.$container.append($info);
+
+    const $footer = $(`
+      <footer>
+        <button class="loc">Location</button>
+        <button class="delay">Delay Notification</button>
+      </footer>
+    `);
+    this.$container.append($footer);
+  }
 }
