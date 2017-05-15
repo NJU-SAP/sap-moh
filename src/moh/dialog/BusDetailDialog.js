ec3b7d60c40f658a9c01fc45956f0e59a67fa06d
Henry Li <henry1943@163.com>
2017-05-15 10:07:58 +0800

    Update

--- src/moh/dialog/BusDetailDialog.js
+++ src/moh/dialog/BusDetailDialog.js
@@ -9,12 +9,5 @@ export default class BusDetailDialog extends Dialog {
   init() {
     super.init();
     this.addStyleClass('moh-bus-detail-dialog col-9 row-7 top-3');
-
-    this._initTitle();
-  }
-
-  _initTitle() {
-    this._$direction = $("<span class='fa fa-arrow-up direction h2'/>");
-    this.$title.append(this._$direction);
   }
 }
