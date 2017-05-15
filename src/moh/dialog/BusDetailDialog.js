810fe1cff8628fd4017121dca53617d1258f0328
Henry Li <henry1943@163.com>
2017-05-15 10:07:33 +0800

    Add title

--- src/moh/dialog/BusDetailDialog.js
+++ src/moh/dialog/BusDetailDialog.js
@@ -8,6 +8,13 @@ import '../res/bus-detail-dialog.less';
 export default class BusDetailDialog extends Dialog {
   init() {
     super.init();
-    this.addStyleClass('moh-bus-detail-dialog');
+    this.addStyleClass('moh-bus-detail-dialog col-9 row-7 top-3');
+
+    this._initTitle();
+  }
+
+  _initTitle() {
+    this._$direction = $("<span class='fa fa-arrow-up direction h2'/>");
+    this.$title.append(this._$direction);
   }
 }
