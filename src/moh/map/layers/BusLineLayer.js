3b5a8c377bc4284bfbe112a1dcb68fccc2e645e2
CompileYouth <CompileYouth@163.com>
2017-05-16 14:22:15 +0800

    modify buslinelayer

---
+++ src/moh/map/layers/BusLineLayer.js
@@ -0,0 +1,15 @@
+import Layer from 'nju/map/layer/Layer';
+
+
+export default class BusLineLayer extends Layer {
+  metadata = {
+    properties: {
+
+    }
+  }
+
+  init() {
+    super.init();
+  }
+
+}
