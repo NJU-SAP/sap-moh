f269079a245633a95df4bc854045ff1706d89e06
CompileYouth <CompileYouth@163.com>
2017-05-16 16:37:08 +0800

    fix bug

--- src/moh/app/Application.js
+++ src/moh/app/Application.js
@@ -314,6 +314,7 @@ export default class Application extends SuperApplication {
     });
     this._busDetailDialog.attachResume(() => {
       StateBus.getInstance().setState('kaaba', false);
+      StateBus.getInstance().setState('kaaba-group', false);
     });
   }
 
