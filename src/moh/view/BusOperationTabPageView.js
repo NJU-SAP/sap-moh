be60b91baff25918c5bdca65702d0bedf04a8a77
Henry Li <henry1943@163.com>
2017-05-17 17:12:30 +0800

    Update dialog

--- src/moh/view/BusOperationTabPageView.js
+++ src/moh/view/BusOperationTabPageView.js
@@ -7,4 +7,42 @@ export default class BusOperationTabPageView extends TabPageView {
       title: { type: 'string', defaultValue: 'Speed' }
     }
   }
+
+  init() {
+    super.init();
+
+    this.addStyleClass('moh-bus-operation-tab-page');
+
+    this._initMain();
+  }
+
+  _initMain() {
+    this.$container.html(`
+      <div class="left-container">
+        <div class="speed-container">
+          <h2>Avg. Speed</h2>
+          <div>
+            <h2 class="value"></h2>
+            <span class="unit">km/h</span>
+          </div>
+        </div>
+        <div class="sunburst-chart-container">
+          Sunburst Chart
+        </div>
+      </div>
+      <div class="right-container">
+        <div class="chart-container">
+          Chart 1
+        </div>
+        <div class="chart-container">
+          Chart 2
+        </div>
+      </div>
+    `);
+  }
+
+  update() {
+    const index = sap.ui.getCore().getModel('index').getProperty('/rt');
+    this.$('.speed-container .value').text(index[index.length - 2].busSpeed);
+  }
 }
