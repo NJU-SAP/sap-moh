b79cbb1608d3db6d9684c2f41464e01a6af38bf9
Henry Li <henry1943@163.com>
2017-05-17 14:13:02 +0800

    Update service and model of district speed

--- src/moh/service/TrafficServiceClient.js
+++ src/moh/service/TrafficServiceClient.js
@@ -19,4 +19,10 @@ export default class TrafficServiceClient extends ServiceClient {
       time
     });
   }
+
+  getDistrictSpeed(time) {
+    return this.fetch('district-speed', {
+      time
+    });
+  }
 }
