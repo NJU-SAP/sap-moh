139cbbb3efbb99376db83115f6cd61058315d27a
Henry Li <henry1943@163.com>
2017-05-18 10:49:36 +0800

    Update url

--- src/bd/map/MapView.js
+++ src/bd/map/MapView.js
@@ -5,7 +5,10 @@ export default class MapView extends SuperMapView {
   initLayers() {
     super.initLayers();
 
-    this.baseLayer = new TileLayer({ url: 'https://api.mapbox.com/styles/v1/mapbox/traffic-night-v2/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiaGVucnkxOTg0IiwiYSI6ImI1a0FvUzQifQ.zLCAzKNjXNiRUQoJBzAsZQ' });
+    this.baseLayer = new TileLayer({
+      // url: 'https://api.mapbox.com/styles/v1/mapbox/traffic-night-v2/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiaGVucnkxOTg0IiwiYSI6ImI1a0FvUzQifQ.zLCAzKNjXNiRUQoJBzAsZQ'
+      url: '/map/{z}/{x}/{y}.png'
+    });
     this.addLayer(this.baseLayer);
   }
 }
